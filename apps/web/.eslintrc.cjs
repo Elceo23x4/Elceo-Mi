743f91b777d43387e9988bf6112920362b1a70d8
Elceo <142925108+Elceo23x4@users.noreply.github.com>
2026-04-16 21:21:53 +0000

    commit

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -16,5 +16,8 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "next/core-web-vitals"
-  ]
+  ],
+  rules: {
+  "@typescript-eslint/no-explicit-any": "off"
+}
 };
